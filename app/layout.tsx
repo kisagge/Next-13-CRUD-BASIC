67dc19ca680e717e616e95cceecba4736f3ca2fd
kisagge <kisagge@naver.com>
2022-11-28 16:05:24 +0900

    layout typescript 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,18 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+import React, { ReactNode } from "react";
+
+const layout = ({ children }: { children: ReactNode }) => {
   return (
     <html>
-      <head />
-      <body>{children}</body>
+      <head>
+        <title>test Next.js 13</title>
+      </head>
+      <body>
+        <header>header</header>
+        <section>{children}</section>
+        <footer>footer</footer>
+      </body>
     </html>
-  )
-}
+  );
+};
+
+export default layout;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
-export default function Page() {
-  return <h1>Hello Next</h1>;
-}
+const page = () => {
+  return <div>test page</div>;
+};
+
+export default page;
